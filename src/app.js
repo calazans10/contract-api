d901578997dea8fb06973bdb49f9a40dcdf1261c
Jeferson Farias Calazans <calazans10@gmail.com>
2020-11-08 11:45:56 -0300

    Add endpoint to pay for a job

--- src/app.js
+++ src/app.js
@@ -13,24 +13,55 @@ app.set('models', sequelize.models)
 app.get('/contracts/:id', getProfile, async (req, res) => {
     const { Contract } = req.app.get('models')
     const { id } = req.params
-    const { id: profile_id } = req.profile
-    const contract = await Contract.findOne({where: {id, [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }] }})
+    const { id: profileId } = req.profile
+    const contract = await Contract.findOne({where: { id, [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }] }})
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
 
 app.get('/contracts', getProfile, async (req, res) => {
     const { Contract } = req.app.get('models')
-    const { id: profile_id } = req.profile
-    const contracts = await Contract.findAll({where: { [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }], status: { [Op.not]: 'terminated' }}})
+    const { id: profileId } = req.profile
+    const contracts = await Contract.findAll({where: { [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }], status: { [Op.not]: 'terminated' }}})
     res.json(contracts)
 })
 
 app.get('/jobs/unpaid', getProfile, async (req, res) => {
     const { Contract, Job } = req.app.get('models')
-    const { id: profile_id } = req.profile
-    const jobs = await Job.findAll({ include: { model: Contract, where: { [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }], status: 'in_progress' }, attributes: []}})
+    const { id: profileId } = req.profile
+    const jobs = await Job.findAll({ include: { model: Contract, where: { [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }], status: 'in_progress' }, attributes: []}})
     res.json(jobs)
+})
+
+app.post('/jobs/:job_id/pay', getProfile, async (req, res) => {
+    const { Contract, Job, Profile } = req.app.get('models')
+    const { job_id: id } = req.params
+    const { profile } = req
+    const job = await Job.findOne({ where: { id }, include: { model: Contract, where: { ClientId: profile.id } }})
+    
+    if(!job) return res.status(404).end()
+    if(job.paid) return res.status(422).end()
+    if(profile.balance < job.price) return res.status(422).end()
+
+    const contractor = await Profile.findOne({ where: { id: job.Contract.ContractorId }})
+
+    const t = await sequelize.transaction();
+
+    try {
+        profile.balance -= job.price
+        await profile.save()
+
+        contractor.balance += job.price
+        await contractor.save()
+
+        job.paymentDate = new Date()
+        job.paid = true
+        await job.save()
+    } catch (error) {
+        await t.rollback();
+    }
+
+    res.status(201).end()
 })
 
 module.exports = app;
