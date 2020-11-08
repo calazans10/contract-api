867323fe4a7a7e2735a72eabaf0521513edeb553
Jeferson Farias Calazans <calazans10@gmail.com>
2020-11-08 10:29:47 -0300

    Fix endpoint that returns a detail of a contract

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
+const { Op } = require("sequelize");
 const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+
 const app = express();
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const { id: profile_id } = req.profile
+    const contract = await Contract.findOne({where: {id, [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }] }})
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
