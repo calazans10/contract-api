38e995ec2abe61e7f6833782940e769667663074
Jeferson Farias Calazans <calazans10@gmail.com>
2020-11-08 10:38:21 -0300

    Add endpoint that returns a list of contracts belonging to a user

--- src/app.js
+++ src/app.js
@@ -10,12 +10,21 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
     const { id: profile_id } = req.profile
     const contract = await Contract.findOne({where: {id, [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }] }})
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
+
+app.get('/contracts', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const { id: profile_id } = req.profile
+    const contracts = await Contract.findAll({where: { [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }], status: { [Op.not]: 'terminated' }}})
+    res.json(contracts)
+})
+
+
 module.exports = app;
